cb2a21577bfa0843c2fb25c7b6cf805a5e75c5b3
Tomás Barreira <tomas.barreira14@outlook.pt>
2020-05-11 22:34:48 +0100

    NavBar indexing update and update to validator

--- src/util/validators.js
+++ src/util/validators.js
@@ -1,3 +1,6 @@
 export const required = (value) => value.trim() !== '';
 
-export const numeric = (value) => {};
+export const numeric = (value) => 
+  /^[0-9]+(\.[0-9]{0,2})?$/.test(
+    value
+  );
